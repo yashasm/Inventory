b887cae9c1461e66508ea171619603942bd5d129
yashasm <yashasm1991@gmail.com>
2017-05-30 16:48:24 -0700

    Added forever module

--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@ apt-get install nodejs
 apt install nodejs-legacy
 apt-get install npm
 npm install
+npm install forever -g
 
 echo "===================================================="
 echo "Project setup succesfull. Please visit http://localhost:3000"
 echo "===================================================="
 
-node app.js
+nohup forever app.js &
